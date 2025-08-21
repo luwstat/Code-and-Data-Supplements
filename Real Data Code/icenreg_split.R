e128511134ded06c157a599f2d83177bba6bce23
luwstat <49051489+luwstat@users.noreply.github.com>
2025-08-21 11:45:40 -0400

    Update icenreg_split.R

--- Real Data Code/icenreg_split.R
+++ Real Data Code/icenreg_split.R
@@ -1,44 +1,64 @@
 library(icenReg)
-####################Read data set and change setting here####################
-#data cleaning
-my.data <- read.table(file = "F:/project1/revise_realdata/C.csv", sep = "," ,header = TRUE)
 
-#mutant type
-my.data <- my.data[my.data[,"KRAS_C"] == 1,]
+## helper to pretty-print a system.time object
+print_timing <- function(tag, tm) {
+  cat(sprintf("\n[%s] timing — elapsed: %.2f s | user: %.2f s | sys: %.2f s\n",
+              tag, tm["elapsed"], tm["user.self"], tm["sys.self"]))
+}
+
+t_total_start <- proc.time()
+
+#################### Read data set ####################
+dat0 <- read.table(file = "F:/project1/revise_realdata/C.csv", sep = "," , header = TRUE)
+
+#################### Mutant type (KRAS_C == 1) ####################
+my.data <- dat0[dat0[,"KRAS_C"] == 1, ]
 n <- nrow(my.data)
 
-L <- rep(NA,n)
-R <- rep(NA,n)
-L[my.data[,'IC'] == 0] <- my.data[my.data[,'IC'] == 0,1]
-R[my.data[,'IC'] == 0] <- my.data[my.data[,'IC'] == 0,1]
-
-L[my.data[,'IC'] == 1] <- my.data[my.data[,'IC'] == 1,"L"]
-R[my.data[,'IC'] == 1] <- my.data[my.data[,'IC'] == 1,"R"]
+L <- R <- rep(NA_real_, n)
+L[my.data[,'IC'] == 0] <- my.data[my.data[,'IC'] == 0, 1]
+R[my.data[,'IC'] == 0] <- my.data[my.data[,'IC'] == 0, 1]
+L[my.data[,'IC'] == 1] <- my.data[my.data[,'IC'] == 1, "L"]
+R[my.data[,'IC'] == 1] <- my.data[my.data[,'IC'] == 1, "R"]
 R[my.data[,"y"] == 2] <- Inf
 
-TRT_C <- my.data[,c("TRT_C")]
-D <- cbind(L,R,TRT_C)
+TRT_C <- my.data[, "TRT_C"]
+D <- cbind(L, R, TRT_C)
 
-fit0 <- ic_sp(cbind(D[,"L"],D[,"R"])~ TRT_C ,model = 'po',data = as.data.frame(D),bs_samples = 100)
-summary(fit0)
+tm_mut <- system.time({
+  fit0 <- ic_sp(cbind(D[,"L"], D[,"R"]) ~ TRT_C,
+                model = 'po',
+                data = as.data.frame(D),
+                bs_samples = 100)
+  s0 <- summary(fit0)
+})
+print(s0)
+print_timing("KRAS mutant (KRAS_C==1)", tm_mut)
 
+#################### Wild type (KRAS_C == 0) ####################
+my.data <- dat0[dat0[,"KRAS_C"] == 0, ]
+n <- nrow(my.data)
 
-#data cleaning
-my.data <- read.table(file = "F:/project1/revise_realdata/C.csv", sep = "," ,header = TRUE)
-#wild type
-my.data <- my.data[my.data[,"KRAS_C"] == 0,]
-
-L <- rep(NA,n)
-R <- rep(NA,n)
-L[my.data[,'IC'] == 0] <- my.data[my.data[,'IC'] == 0,1]
-R[my.data[,'IC'] == 0] <- my.data[my.data[,'IC'] == 0,1]
-
-L[my.data[,'IC'] == 1] <- my.data[my.data[,'IC'] == 1,"L"]
-R[my.data[,'IC'] == 1] <- my.data[my.data[,'IC'] == 1,"R"]
+L <- R <- rep(NA_real_, n)
+L[my.data[,'IC'] == 0] <- my.data[my.data[,'IC'] == 0, 1]
+R[my.data[,'IC'] == 0] <- my.data[my.data[,'IC'] == 0, 1]
+L[my.data[,'IC'] == 1] <- my.data[my.data[,'IC'] == 1, "L"]
+R[my.data[,'IC'] == 1] <- my.data[my.data[,'IC'] == 1, "R"]
 R[my.data[,"y"] == 2] <- Inf
 
-TRT_C <- my.data[,c("TRT_C")]
-D <- cbind(L,R,TRT_C)
+TRT_C <- my.data[, "TRT_C"]
+D <- cbind(L, R, TRT_C)
 
-fit1 <- ic_sp(cbind(D[,"L"],D[,"R"])~ TRT_C ,model = 'po',data = as.data.frame(D),bs_samples = 100)
-summary(fit1)
+tm_wild <- system.time({
+  fit1 <- ic_sp(cbind(D[,"L"], D[,"R"]) ~ TRT_C,
+                model = 'po',
+                data = as.data.frame(D),
+                bs_samples = 100)
+  s1 <- summary(fit1)
+})
+print(s1)
+print_timing("KRAS wild-type (KRAS_C==0)", tm_wild)
+
+#################### Total timing ####################
+t_total <- proc.time() - t_total_start
+print_timing("Total pipeline", t_total)
